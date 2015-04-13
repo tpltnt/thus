cc162a4a05c0117800292f4619808372f67d2bab
tpltnt <tpltnt.github@dropcut.net>
2015-04-13 23:18:54 +0200

    added upload handler

--- thus.go
+++ thus.go
@@ -1,33 +1,67 @@
 package main
 
 import (
-       "io"
-       "flag"
-       "log"
-       "net/http"
-       "strconv"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 // Serve the HTML form to upload a file
 func FormServer(w http.ResponseWriter, req *http.Request) {
-     if "GET" != req.Method {
-        log.Fatal("unsupported request method")
-     }
-     io.WriteString(w, "<html><header></header><body>\n")
-     io.WriteString(w, "<form action=\"http://localhost:8080/receive\" method=\"post\" enctype=\"multipart/form-data\">")
-     io.WriteString(w, "<label for=\"file\">filename:</label>")
-     io.WriteString(w, "<input type=\"file\" name=\"file\" id=\"upfile\">")
-     io.WriteString(w, "<input type=\"submit\" name=\"submit\" value=\"submit\">")
-     io.WriteString(w, "</form></body></html>\n")
+	if "GET" != req.Method {
+		log.Fatal("unsupported request method")
+	}
+	io.WriteString(w, "<html><header></header><body>\n")
+	io.WriteString(w, "<form action=\"http://localhost:8080/receive\" method=\"post\" enctype=\"multipart/form-data\">")
+	io.WriteString(w, "<label for=\"file\">filename:</label>")
+	io.WriteString(w, "<input type=\"file\" name=\"file\" id=\"file\">")
+	io.WriteString(w, "<input type=\"submit\" name=\"submit\" value=\"submit\">")
+	io.WriteString(w, "</form></body></html>\n")
+}
+
+// Handle the file upload
+func uploadHandler(w http.ResponseWriter, req *http.Request) {
+	if "POST" != req.Method {
+		log.Fatal("unsupported request method for file upload")
+	}
+
+	// get file by ID from POST request
+	file, header, err := req.FormFile("file")
+	if err != nil {
+		log.Fatal("error getting file from form data: ", err)
+	}
+	defer file.Close()
+
+	// try to create file handler
+	out, err := os.Create("/tmp/uploaded_file")
+	if err != nil {
+		log.Fatal("Could not create file. Maybe wrong privileges?")
+	}
+	defer out.Close()
+
+	// write content of file to disk
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// indicate success
+	io.WriteString(w, "<html><header></header><body>sucessfully uploaded ")
+	io.WriteString(w, header.Filename)
+	io.WriteString(w, "</body></html>")
 }
 
 func main() {
-     var port = flag.Int("port", 8080, "port to listen on (default: 8080)")
-     flag.Parse()
-     log.Println("listening on port " + strconv.Itoa(*port))
-     http.HandleFunc("/", FormServer)
-	 err := http.ListenAndServe(":" + strconv.Itoa(*port), nil)
-	 if err != nil {
-        log.Fatal("server failed: ", err)
-     }
+	var port = flag.Int("port", 8080, "port to listen on (default: 8080)")
+	flag.Parse()
+	log.Println("listening on port " + strconv.Itoa(*port))
+	http.HandleFunc("/", FormServer)
+	http.HandleFunc("/receive", uploadHandler)
+	err := http.ListenAndServe(":"+strconv.Itoa(*port), nil)
+	if err != nil {
+		log.Fatal("server failed: ", err)
+	}
 }
