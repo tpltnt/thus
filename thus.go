a9d72abaaebd2a979ab29392d6ab9b3a1cea3482
tpltnt <tpltnt.github@dropcut.net>
2015-04-13 22:52:48 +0200

    actually serving a file upload form

--- thus.go
+++ thus.go
@@ -8,19 +8,24 @@ import (
        "strconv"
 )
 
-// upload page
-func HelloServer(w http.ResponseWriter, req *http.Request) {
+// Serve the HTML form to upload a file
+func FormServer(w http.ResponseWriter, req *http.Request) {
      if "GET" != req.Method {
         log.Fatal("unsupported request method")
      }
-     io.WriteString(w, "<html><header></header><body>hi there</body></html>\n")
+     io.WriteString(w, "<html><header></header><body>\n")
+     io.WriteString(w, "<form action=\"http://localhost:8080/receive\" method=\"post\" enctype=\"multipart/form-data\">")
+     io.WriteString(w, "<label for=\"file\">filename:</label>")
+     io.WriteString(w, "<input type=\"file\" name=\"file\" id=\"upfile\">")
+     io.WriteString(w, "<input type=\"submit\" name=\"submit\" value=\"submit\">")
+     io.WriteString(w, "</form></body></html>\n")
 }
 
 func main() {
      var port = flag.Int("port", 8080, "port to listen on (default: 8080)")
      flag.Parse()
      log.Println("listening on port " + strconv.Itoa(*port))
-     http.HandleFunc("/", HelloServer)
+     http.HandleFunc("/", FormServer)
 	 err := http.ListenAndServe(":" + strconv.Itoa(*port), nil)
 	 if err != nil {
         log.Fatal("server failed: ", err)
